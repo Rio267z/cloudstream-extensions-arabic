d4f1b3f40c5e2b11216fe31cabdd1f1ee1081782
Rio267z <abd5u743r577@gmail.com>
2024-12-20 09:28:56 +0200

    Update build.gradle.kts

--- ArabSeedProvider/build.gradle.kts
+++ ArabSeedProvider/build.gradle.kts
@@ -1,13 +1,13 @@
-version = 3
+version = 4
 
 cloudstream {
-    authors = listOf( "ImZaw" )
+    authors = listOf( "Rio267z" )
 
     language = "ar"
 	
     status = 1
 
-    tvTypes = listOf( "TvSeries" , "Movie" )
+    tvTypes = listOf( "مسلسلات" , "أفلام" )
 
     iconUrl = "https://www.google.com/s2/favicons?domain=arabseed.ink&sz=%size%"
 }
